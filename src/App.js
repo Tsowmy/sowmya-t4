818ce50bbc410b500538551953ef65deec69b2ff
sowmya <r190157@rguktrkv.ac.in>
2024-06-01 16:52:11 +0530

    Removed Routing

--- src/App.js
+++ src/App.js
@@ -7,30 +7,14 @@ import Login from "./Login.js"
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 
 
-function Header(){
-  return(
-    <ul>
-       <li> <link to="/">Home</link> </li>
-       <li> <link to="/about">About</link> </li>
-       <li> <link to="/contact">Contact</link> </li>
-       <li> <link to="/login">Login</link> </li>
-    </ul>
+  
 
- )
-} 
 function App(){
   return(
     <> 
-      <BrowserRouter>
-          <Header/>
-          <div>Router</div>
-          <Routes>
-             <Route path="/" element={<Home/>}/ >
-             <Route path="/about" element={<About/>}/>
-             <Route path="/contact" element={<Contact/>}/>
-             <Route path="/login" element={<Login/>}/>
-          </Routes>
-      </BrowserRouter>
+      <main>
+        <h2>Welcome to the world</h2>
+        </main>
     </>
   );    
 }
